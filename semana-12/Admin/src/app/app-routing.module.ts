d38439fdc47c464d9c7f3084b95d426710d285d9
WilliamDelcid <dm18019@ues.edu.sv>
2023-07-16 21:18:17 -0600

    FullCalendar\

--- semana-12/Admin/src/app/app-routing.module.ts
+++ semana-12/Admin/src/app/app-routing.module.ts
@@ -8,18 +8,18 @@ import { Page404Component } from './extrapages/page404/page404.component';
 import { IsActiveGuard } from './core/guards/is-active.guard';
 
 const routes: Routes = [
-  { path: 'account', loadChildren: () => import('./account/account.module').then(m => m.AccountModule),canActivate:[IsActiveGuard] },
+  { path: 'account', loadChildren: () => import('./account/account.module').then(m => m.AccountModule), },
   // tslint:disable-next-line: max-line-length
-  { path: '', component: LayoutComponent, loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule),canActivate:[AuthGuard],canLoad:[AuthGuard] },
-  { path: 'pages', loadChildren: () => import('./extrapages/extrapages.module').then(m => m.ExtrapagesModule),canActivate:[AuthGuard],canLoad:[AuthGuard] },
-  { path: 'anime', component: LayoutComponent, loadChildren: () => import('./modules/anime/anime.module').then(m => m.AnimeModule),canActivate:[AuthGuard],canLoad:[AuthGuard]},
-  { path: 'empleados', component: LayoutComponent, loadChildren: () => import('./modules/empleado/empleado.module').then(m => m.EmpleadoModule),canActivate:[AuthGuard],canLoad:[AuthGuard] },
-  { path: 'excel', component: LayoutComponent, loadChildren: () => import('./modules/export-excel/export-excel.module').then(m => m.ExportExcelModule),canActivate:[AuthGuard],canLoad:[AuthGuard] },
-  { path: 'bibliotecas', component: LayoutComponent, loadChildren: () => import('./modules/bibliotecas/bibliotecas.module').then(m => m.BibliotecasModule),canActivate:[AuthGuard],canLoad:[AuthGuard] },
-  { path: 'libro', component: LayoutComponent, loadChildren: () => import('./modules/libro/libro.module').then(m => m.LibroModule),canActivate:[AuthGuard],canLoad:[AuthGuard] },
-  { path: 'clinica', component: LayoutComponent, loadChildren: () => import('./modules/clinica/clinica.module').then(m => m.ClinicaModule),canActivate:[AuthGuard],canLoad:[AuthGuard] },
-  { path: 'exportar', component: LayoutComponent, loadChildren: () => import('./modules/exportar/exportar.module').then(m => m.ExportarModule),canActivate:[AuthGuard],canLoad:[AuthGuard] },
-  { path: 'crypto-ico-landing', component: CyptolandingComponent,canActivate:[AuthGuard],canLoad:[AuthGuard] },
+  { path: '', component: LayoutComponent, loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule), },
+  { path: 'pages', loadChildren: () => import('./extrapages/extrapages.module').then(m => m.ExtrapagesModule), },
+  { path: 'anime', component: LayoutComponent, loadChildren: () => import('./modules/anime/anime.module').then(m => m.AnimeModule),},
+  { path: 'empleados', component: LayoutComponent, loadChildren: () => import('./modules/empleado/empleado.module').then(m => m.EmpleadoModule), },
+  { path: 'excel', component: LayoutComponent, loadChildren: () => import('./modules/export-excel/export-excel.module').then(m => m.ExportExcelModule), },
+  { path: 'bibliotecas', component: LayoutComponent, loadChildren: () => import('./modules/bibliotecas/bibliotecas.module').then(m => m.BibliotecasModule), },
+  { path: 'libro', component: LayoutComponent, loadChildren: () => import('./modules/libro/libro.module').then(m => m.LibroModule), },
+  { path: 'clinica', component: LayoutComponent, loadChildren: () => import('./modules/clinica/clinica.module').then(m => m.ClinicaModule), },
+  { path: 'exportar', component: LayoutComponent, loadChildren: () => import('./modules/exportar/exportar.module').then(m => m.ExportarModule), },
+  { path: 'crypto-ico-landing', component: CyptolandingComponent, },
   { path: '**', component: Page404Component },
 ];
 
--- semana-12/Admin/src/app/modules/exportar/exportar.module.ts
+++ semana-12/Admin/src/app/modules/exportar/exportar.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ExportarComponent } from './pages/exportar/exportar.component';
 import { ExportarRoutingModule } from './exportar-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DROPZONE_CONFIG, DropzoneConfigInterface, DropzoneModule } from 'ngx-dropzone-wrapper';
-
+import { FullCalendarModule } from '@fullcalendar/angular';
+import {ModalModule} from 'ngx-bootstrap/modal'
 const DEFAULT_DROPZONE_CONFIG: DropzoneConfigInterface = {
   // Change this to your upload POST address:
   url: 'http://localhost:8080/file/upload',
@@ -16,7 +18,8 @@ const DEFAULT_DROPZONE_CONFIG: DropzoneConfigInterface = {
     ExportarComponent
   ],
   imports: [
-    CommonModule,ExportarRoutingModule,DropzoneModule,
+    CommonModule,ExportarRoutingModule,DropzoneModule,ReactiveFormsModule,FullCalendarModule,ModalModule.forRoot(),FormsModule
+
   ],
   providers: [
     {
